1341e39ae471e3374839b6ebbaa677488621b286
Ydnil-poh <warpart@naver.com>
2026-06-08 14:25:55 +0900

    Update remark-youtube.js

--- src/plugins/remark-youtube.js
+++ src/plugins/remark-youtube.js
@@ -1,18 +1,9 @@
 import { visit } from 'unist-util-visit';
 
-const YOUTUBE_RE = /^!youtube\s+(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/watch\?v=)([\w-]+)/;
-
 export default function remarkYoutube() {
   return (tree) => {
     visit(tree, 'paragraph', (node) => {
-      const text = node.children?.[0]?.value;
-      if (!text) return;
-
-      const match = text.match(YOUTUBE_RE);
-      if (!match) return;
-
-      node.type = 'html';
-      node.value = `<YouTubeEmbed videoId="${match[1]}" />`;
+      console.log(JSON.stringify(node, null, 2));
     });
   };
 }
