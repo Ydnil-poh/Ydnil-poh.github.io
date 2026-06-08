cc3636340d5092a8ecb5979f1c694eae29d4a11f
Ydnil-poh <warpart@naver.com>
2026-06-08 14:16:32 +0900

    Configure YouTube markdown embeds

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import remarkYoutube from './src/plugins/remark-youtube.js';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ydnil-poh.github.io',
+  markdown: {
+    remarkPlugins: [remarkYoutube]
+  }
 });
