9c0b43239a0263e4e31e4cc3d5cd2e4f1d566c01
Ydnil-poh <warpart@naver.com>
2026-07-10 14:27:16 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import remarkYoutube from './src/plugins/remark-youtube.js';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://ydnil-poh.github.io',
+
+  integrations: [
+    sitemap()
+  ],
+
   markdown: {
     remarkPlugins: [remarkYoutube]
   }
